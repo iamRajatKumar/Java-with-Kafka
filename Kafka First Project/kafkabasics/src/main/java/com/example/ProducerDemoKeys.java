2382669ad2ebb820ed8a9d75d084338c96f5f0a7
iamRajatKumar <rohit.r.sharma2@gmail.com>
2026-06-09 23:19:51 +0530

    Producer java unused code removed

--- Kafka First Project/kafkabasics/src/main/java/com/example/ProducerDemoKeys.java
+++ Kafka First Project/kafkabasics/src/main/java/com/example/ProducerDemoKeys.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RoundRobinPartitioner;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
